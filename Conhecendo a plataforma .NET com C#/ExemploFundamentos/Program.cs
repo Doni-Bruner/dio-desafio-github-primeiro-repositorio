0f95d32c1c8f8b3449c653766140011d7c16cd64
Doni-Bruner <donibruner@gmail.com>
2023-01-19 22:02:12 -0300

    Conhecendo tipos de variaveis e operadores.

--- Conhecendo a plataforma .NET com C#/ExemploFundamentos/Program.cs
+++ Conhecendo a plataforma .NET com C#/ExemploFundamentos/Program.cs
@@ -1,3 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
+﻿using ExemploFundamentos.Models;
+
+int a = 5;
+double b = a;
+
+Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+
+// Cast - Casting
+// int a = Convert.ToInt32("5"); // aceita valores nulo, nao geram erro no programa
+// int a = int.Parse("5"); // não aceita valores nulo, gera erro no programa
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c = c + 5;
+// c += 5;
+
+// Console.WriteLine(c);
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual.ToString("dd/MM/yy"));
+
+
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("valor da variavel quantidade: " + quantidade);
+// Console.WriteLine("valor da variavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("valor da variavel preco: " + preco);
+// Console.WriteLine("valor da variavel condição: " + condicao);
+
+
+
+
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Doni";
+//pessoa1.Idade = 29;
+//pessoa1.Apresentar();

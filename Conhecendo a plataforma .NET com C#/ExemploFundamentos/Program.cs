20757d8ba4884df62e8f275f69cdcb74e7641235
Doni-Bruner <donibruner@gmail.com>
2023-01-23 21:50:58 -0300

    Conclusão da aula de operadores C#

--- Conhecendo a plataforma .NET com C#/ExemploFundamentos/Program.cs
+++ Conhecendo a plataforma .NET com C#/ExemploFundamentos/Program.cs
@@ -1,9 +1,200 @@
 ﻿using ExemploFundamentos.Models;
 
-int a = 5;
-double b = a;
+bool choveu = true;
+bool estaTarde = true;
 
-Console.WriteLine(b);
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar!");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia!");
+}
+
+
+
+
+
+
+
+
+
+
+
+// bool possuiPresencaMinima = true;
+// double media = 7.5;
+
+// if (possuiPresencaMinima && media >= 7)
+// {
+//     Console.WriteLine("Aprovado!");
+// }
+// else
+// {
+//     Console.WriteLine("Reprovado!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool ehMaiorDeIdade = true;
+// bool possuiAutorizacaoDoResponsavel = false;
+
+// if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+// {
+//     Console.WriteLine("Entrada Autorizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Entrada não Autorizada!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Digite uma letra: ");
+// string letra = Console.ReadLine();
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine("Vogal!");
+//         break;
+
+//     default:
+//         Console.WriteLine("Não é uma Vogal!");
+//         break;    
+// }
+
+
+
+
+
+
+
+
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 4;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda invalida!");
+// }
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda Realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe, Não temos a quantidade desejada em estoque!");
+// }
+
+
+
+
+
+
+
+
+
+
+// string a = "15";
+
+// int b = 0;
+
+// int.TryParse(a, out b);
+
+// Console.WriteLine(b);
+// Console.WriteLine("Conversão realizada com sucesso.");
+
+
+
+
+
+
+
+
+
+
+
+// double a = 4 / 2 + 2;
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+
+
+// int a = 5;
+// double b = a;
+
+// Console.WriteLine(b);
 
 
 
